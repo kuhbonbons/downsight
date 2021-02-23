c632a4550f9ace6032a38c0be9e87eaaebcef641
niceFont <peterwilms@hotmail.de>
2021-02-23 21:31:50 +0100

    Changed service loading

--- lib/downsight/service_checker.ex
+++ lib/downsight/service_checker.ex
@@ -1,22 +1,23 @@
 defmodule Downsight.ServiceChecker do
   alias Downsight.Repo
   import Ecto.Query
-  import ExAws
+  require Logger
   use GenServer
 
 
   @queue_url Application.fetch_env!(:downsight, :sqs_queue)
   def start_link(_opts) do
-    IO.puts "Starting ServiceChecker"
+    Logger.info("Starting ServiceChecker")
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   # Implement a way to retry failed messages
   defp load_services do
+    Logger.info("Loading Services...")
     with op <- ExAws.SQS.receive_message(@queue_url, max_number_of_messages: 10, wait_time_seconds: 20),
     %{body: %{messages: messages}} <- ExAws.request!(op)
     do
-      sevices = messages
+      services = messages
       |> Enum.map(fn %{body: body} ->
         {:ok, service} = Jason.decode(body)
         service
@@ -26,21 +27,21 @@ defmodule Downsight.ServiceChecker do
       end)
       ExAws.SQS.delete_message_batch(@queue_url, delete_batch)
       |> ExAws.request()
-      sevices
+      Logger.debug(services)
+      services
     else
       _ -> IO.puts("oops")
     end
 
   end
   def check(%{"id" => id, "url" => url, "port" => port, "method" => method, "headers" => headers}) do
+    Logger.info("Sending Requests...")
     url = URI.parse(url)
     url = %URI{url | port: port}
     headers = Jason.decode!(headers)
     {_, %Finch.Response{status: status}} =
       Finch.build(String.to_atom(method), URI.to_string(url), headers, nil)
       |> Finch.request(FinchClient)
-
-    IO.inspect(status, label: "STATUS")
     case status do
       x when x >= 200 and x < 300 ->
         status_compare(:success, id)
@@ -51,30 +52,29 @@ defmodule Downsight.ServiceChecker do
   end
 
   def status_compare(:success, id) do
-    IO.puts "Request with ID #{id} successful"
+    Logger.debug("Request with ID #{id} was successful")
     res =
       Repo.all(
         from s in Downsight.Service,
           where: s.id == ^id
       )
-    IO.inspect(res)
+    Logger.debug(res)
   end
 
   def status_compare(:fail, id) do
-    IO.puts "Request with ID #{id} failed"
+    Logger.debug("Request with ID #{id} has failed")
     res =
       Repo.all(
         from s in Downsight.Service,
           where: s.id == ^id
       )
-    IO.inspect(res)
+    Logger.debug(res)
   end
 
   @impl true
   def init(_opts) do
-    services = load_services()
     Process.send_after(self(), :tick, 10000)
-    {:ok, services}
+    {:ok, []}
   end
 
 
@@ -83,22 +83,25 @@ defmodule Downsight.ServiceChecker do
     {:reply, nil, state ++ [service]}
   end
 
+  def handle_call(:load_services, _from, _state) do
+    services = load_services()
+    {:reply, nil, services}
+  end
+
   @impl true
   def handle_cast(:check_services, state) do
-    IO.inspect(state, label: "Current state")
-
     Enum.map(state, fn service ->
       Task.async(fn -> check(service) end)
     end)
     |> Task.await_many()
-    {:noreply, state}
+    {:noreply, []}
   end
 
   @impl true
-  def handle_info(:tick, state) do
-    IO.puts "Next Tick"
+  def handle_info(:tick, _state) do
+    services = load_services()
     GenServer.cast(__MODULE__, :check_services)
     Process.send_after(self(), :tick, 10000)
-    {:noreply, state}
+    {:noreply, services}
   end
 end
